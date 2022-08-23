085cba25731b5809f4f47d104e09c240f6efed44
samuelran <samuelbesserransby@gmail.com>
2022-08-23 10:21:13 +0200

    hehe

---
+++ src/main/java/com/example/helloworldzzz/HelloworldzzzApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldzzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldzzzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldzzzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helloworldzzz/HelloworldzzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldzzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldzzzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
